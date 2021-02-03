f9cea1dd38ab20068d62b21a36d4de60d049e4e2
Sindhu <psindhura1496@gmail.com>
2021-02-03 16:34:37 +0530

    Added a project

---
+++ src/main/java/com/example/sample/codedemo/CodedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample.codedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodedemoApplication.class, args);
+	}
+
+}
